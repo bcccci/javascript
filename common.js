67b2c17e8e8c4c4cf491a14dba4fdfee7b0adc37
bcccci <35388494+bcccci@users.noreply.github.com>
2018-01-22 10:40:42 +0800

    javascript

---
+++ common.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2018/1/22.
+ */
+document.write("<h1>hhhhh</h1>");
+document.write("<h1>hhhhh</h1>");
+document.write("<h1>hhhhh</h1>");
+document.write("<h1>hhhhh</h1>");
