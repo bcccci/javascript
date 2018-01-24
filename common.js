4ff7e608ce64fe0f95b42902063ebb69344d8fcc
bcccci <35388494+bcccci@users.noreply.github.com>
2018-01-24 18:16:09 +0800

    javascript

--- common.js
+++ common.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2018/1/22.
  */
-document.write("<h1>hhhhh</h1>");
-document.write("<h1>hhhhh</h1>");
-document.write("<h1>hhhhh</h1>");
-document.write("<h1>hhhhh</h1>");
+document.write("<h1>hello world</h1>");
+document.write("<h1>hello world</h1>");
+document.write("<h1>hello world</h1>");
+document.write("<h1>hello world</h1>");
